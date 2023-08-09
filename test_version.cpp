3b3e28d766e421ebec589e104fa0f53160c80744
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-08-09 17:15:01 +0500

    Поменяна библиотека тестирования на GoogleTest

--- test_version.cpp
+++ test_version.cpp
@@ -2,13 +2,13 @@
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+TEST(SuiteName, Name) {
+    EXPECT_GT(version(), 0);
+}
