921f7d9b89e3b473a8cbc6a2c485b1140b0d08ac
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-10 23:19:53 +0500

    accepted

--- main.cpp
+++ main.cpp
@@ -3,19 +3,11 @@
 #include <iomanip>
 #include <iostream>
 
-void printSqrt(std::istream& is) {
-    uint64_t res = 0;
-    if (is >> res) {
-        printSqrt(is);
-        std::cout << std::sqrt(res) << std::endl;
-    }
-}
-
 int main(int, char**) {
-    int a, b, c;
+    int a, b;
 
-    std::cin >> a >> b >> c;
-    std::cout << a * b * c * 2 << std::endl;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
 
     return 0;
 }
