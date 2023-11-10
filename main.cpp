618ad693f3a971b6fcd496bae584db5007a858cf
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-10 23:08:32 +0500

    brute force

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
+#include <cmath>
+#include <cstdint>
+#include <iomanip>
 #include <iostream>
 
-#include "proj_info.h"
+void printSqrt(std::istream& is) {
+    uint64_t res = 0;
+    if (is >> res) {
+        printSqrt(is);
+        std::cout << std::sqrt(res) << std::endl;
+    }
+}
 
-int main(int, char **) {
-    project_info::printProjectInfo();
+int main(int, char**) {
+    std::cout << std::fixed << std::setprecision(4);
+
+    printSqrt(std::cin);
+
     return 0;
 }
