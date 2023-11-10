06b8fe5d27282637a96c8dde756999aa551b53dc
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-10 21:52:34 +0500

    WIP

--- allocator.h
+++ allocator.h
@@ -6,20 +6,75 @@
 
 namespace custom {
 
+#if 0
+template <typename T>
+class Allocator {
+   public:
+    using value_type = T;
+
+    Allocator() noexcept = default;
+    ~Allocator() { operator delete[](pool_); }
+
+    template <typename U, size_t SizeOther>
+    Allocator(const Allocator<U>&) noexcept {}
+
+    value_type* allocate(std::size_t size);
+
+    inline void deallocate([[maybe_unused]]value_type* pValue, std::size_t size) {
+        cap_ -= size;
+    }
+
+    using propagate_on_container_copy_assignment = std::true_type;
+    using propagate_on_container_move_assignment = std::true_type;
+    using propagate_on_container_swap = std::true_type;
+
+   private:
+    inline value_type* allocate_(std::size_t size) { return static_cast<T*>(operator new(sizeof(T) * size)); }
+
+    value_type* pool_ = nullptr;
+    size_t allocSize_ = 0, cap_ = 0;
+};
+
+template <typename T>
+bool operator!=([[maybe_unused]]Allocator<T> first, [[maybe_unused]]Allocator<T> second) {
+    return false;
+}
+
+template <typename T>
+T* Allocator<T>::allocate(std::size_t size) {
+    if (cap_ + size >= allocSize_) {
+        const auto newPoolSize = (cap_ + size) % 4 + cap_ + size;
+
+        auto newPool = allocate_(newPoolSize);
+        std::memcpy(newPool, pool_, allocSize_);
+        operator delete[](pool_);
+        pool_ = newPool;
+        allocSize_ = newPoolSize;
+    }
+
+    auto res = pool_ + cap_;
+    cap_ += size;
+
+    return res;
+}
+#else
+
 template <typename T, size_t Size>
 class Allocator {
    public:
     using value_type = T;
 
     Allocator() noexcept = default;
-    ~Allocator() { operator delete(pool_); }
+    ~Allocator() { operator delete[](pool_); }
 
     template <typename U, size_t SizeOther>
     Allocator(const Allocator<U, SizeOther>&) noexcept {}
 
     value_type* allocate(std::size_t size);
 
-    inline void deallocate(value_type* pValue, [[maybe_unused]] std::size_t) { allocSize_ = cap_ = 0; }
+    inline void deallocate([[maybe_unused]]value_type* pValue, std::size_t size) {
+        cap_ -= size;
+    }
 
     using propagate_on_container_copy_assignment = std::true_type;
     using propagate_on_container_move_assignment = std::true_type;
@@ -33,23 +88,30 @@ class Allocator {
 };
 
 template <typename T, size_t Size>
-T* Allocator<T, Size>::allocate(std::size_t size) {
-    T* res = nullptr;
-    if (cap_ + size >= allocSize_) {
-        const auto newPoolSize = (cap_ + size) % Size;
+bool operator!=([[maybe_unused]]Allocator<T, Size> first, [[maybe_unused]]Allocator<T, Size> second) {
+    return false;
+}
 
-        auto newPool = allocate_(allocSize_);
+template <typename T, size_t Size>
+T* Allocator<T, Size>::allocate(std::size_t size) {
+    if (cap_ + size >= allocSize_) {
+        const auto newPoolSize = (cap_ + size) % Size + cap_ + size;
+
+        auto newPool = allocate_(newPoolSize);
         std::memcpy(newPool, pool_, allocSize_);
-        deallocate();
+        operator delete[](pool_);
+        pool_ = newPool;
+        allocSize_ = newPoolSize;
     }
 
-    res = pool_ + cap_;
+    auto res = pool_ + cap_;
     cap_ += size;
 
     return res;
 }
 
-template <typename Value, typename Allocator = custom::Allocator<Value>>
+template <typename Value, size_t Size, typename Allocator = custom::Allocator<Value, Size>>
 class Vector {};
+#endif
 
 }  // namespace custom
--- main.cpp
+++ main.cpp
@@ -6,12 +6,30 @@
 
 #include "proj_info.h"
 
+//template <typename Key, typename Value, std::size_t Size>
+//using MapAllocator = custom::Allocator<std::pair<const Key, Value>, Size>;
+
 int main(int, char **) {
     project_info::printProjectInfo();
 
-    std::map<int, int, std::less<int>, custom::Allocator<std::pair<const int, int>>> m{};
+    // создание экземпляра std::map<int, int>
+    std::vector<int, custom::Allocator<int, 1000>> v{};
+//    std::map<int, int, std::less<int>, MapAllocator<int, int, 1000>> m{};
+//    std::map<int, int, std::less<int>> m{};
 
-    m.emplace(1, 1);
+    // заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+    // создание экземпляра std::map<int, int> с новым аллокатором, ограниченным 10
+    // элементами
+    // заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+    // вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
+    // создание экземпляра своего контейнера для хранения значений типа int
+    // заполнение 10 элементами от 0 до 9
+    // создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором,
+    // ограниченным 10 элементами
+    // заполнение 10 элементами от 0 до 9
+    // вывод на экран всех значений, хранящихся в контейнере
+
+//    m.emplace(1, 1);
 
     return 0;
 }
