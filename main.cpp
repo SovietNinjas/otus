6bce490b7a881a41e6e16b50b94a68f560ae21e4
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-12 23:36:29 +0500

    brute force

--- main.cpp
+++ main.cpp
@@ -1,20 +1,41 @@
+#include <algorithm>
+#include <array>
 #include <cmath>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
-
-static constexpr int maxCount = 4 * 60 / 45;
+#include <iterator>
+#include <numeric>
+#include <vector>
 
 int main(int, char**) {
-    auto i = 0;
+    std::size_t count = 0;
 
-    while (std::cin >> i) {
-        if (12 - i <= maxCount) {
-            std::cout << "YES" << std::endl;
-        } else {
-            std::cout << "NO" << std::endl;
+    std::vector<std::vector<std::size_t>> nums;
+    nums.reserve(3);
+
+    for (std::size_t i = 0; i < 3; ++i) {
+        std::size_t numCount;
+        std::cin >> numCount;
+        std::cin.ignore(1);
+
+        std::string line;
+        std::getline(std::cin, line);
+        std::istringstream is(std::move(line));
+        nums.push_back(std::vector<std::size_t>(std::istream_iterator<std::size_t>(is), std::istream_iterator<std::size_t>()));
+    }
+
+    for (const auto& i : nums[0]) {
+        auto it = std::lower_bound(begin(nums[1]), end(nums[1]), i);
+        if (it != nums[1].end() && *it == i) {
+            it = std::lower_bound(begin(nums[2]), end(nums[2]), i);
+            if (it != nums[2].end() && *it == i) {
+                ++count;
+            }
         }
     }
+
+    std::cout << count << std::endl;
 
     return 0;
 }
