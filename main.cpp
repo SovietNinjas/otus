ce0c8c1df88659c077636a3d06fc98bc67e70c66
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-12 16:36:03 +0500

    Grisha

--- main.cpp
+++ main.cpp
@@ -3,28 +3,16 @@
 #include <iomanip>
 #include <iostream>
 
+static constexpr int maxCount = 4 * 60 / 45;
+
 int main(int, char**) {
     auto i = 0;
 
     while (std::cin >> i) {
-        if (i >= 1 && i <= 4) {
-            std::cout << "few" << std::endl;
-        } else if (i >= 5 && i <= 9) {
-            std::cout << "several" << std::endl;
-        } else if (i >= 10 && i <= 19) {
-            std::cout << "pack" << std::endl;
-        } else if (i >= 20 && i <= 49) {
-            std::cout << "lots" << std::endl;
-        } else if (i >= 50 && i <= 99) {
-            std::cout << "horde" << std::endl;
-        } else if (i >= 100 && i <= 249) {
-            std::cout << "throng" << std::endl;
-        } else if (i >= 250 && i <= 499) {
-            std::cout << "swarm" << std::endl;
-        } else if (i >= 500 && i <= 999) {
-            std::cout << "zounds" << std::endl;
-        } else if (i >= 1000) {
-            std::cout << "legion" << std::endl;
+        if (12 - i <= maxCount) {
+            std::cout << "YES" << std::endl;
+        } else {
+            std::cout << "NO" << std::endl;
         }
     }
 
