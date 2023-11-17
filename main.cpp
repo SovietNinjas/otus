ccfaf6dcfec8bd03a5c0785306b560e335c72ea9
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-17 13:58:34 +0500

    done

--- main.cpp
+++ main.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
 
+bool isAvailable(char a, char b)  {
+    return a >= 'a' && a <= 'h' && b >= '1' && b <= '8';
+}
+
 int main(int, char**) {
-    std::size_t querryCount, guestCount;
+    std::size_t querryCount;
     std::cin >> querryCount;
     std::cin.ignore(1);
-    guestCount = querryCount + 2;
 
     std::string line;
     while (querryCount) {
+        std::size_t availableSteps = 0;
+
         std::getline(std::cin, line);
-        if (line.find("+one") != line.npos) {
-            ++guestCount;
+
+        if (isAvailable(line[0] + 1, line[1] + 2)) {
+            ++availableSteps;
+        }
+        if (isAvailable(line[0] + 1, line[1] - 2)) {
+            ++availableSteps;
+        }
+        if (isAvailable(line[0] - 1, line[1] + 2)) {
+            ++availableSteps;
+        }
+        if (isAvailable(line[0] - 1, line[1] - 2)) {
+            ++availableSteps;
+        }
+        if (isAvailable(line[0] + 2, line[1] - 1)) {
+            ++availableSteps;
+        }
+        if (isAvailable(line[0] + 2, line[1] + 1)) {
+            ++availableSteps;
+        }
+        if (isAvailable(line[0] - 2, line[1] + 1)) {
+            ++availableSteps;
+        }
+        if (isAvailable(line[0] - 2, line[1] - 1)) {
+            ++availableSteps;
         }
 
+        std::cout << availableSteps << '\n';
 
         --querryCount;
     }
-
-    if (guestCount == 13) {
-        ++guestCount;
-    }
-
-    std::cout << guestCount * 100 << std::endl;
-
+    std::cout << std::flush;
 
     return 0;
 }
