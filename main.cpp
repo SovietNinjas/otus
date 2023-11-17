bc1f8e8af942823d6c49f7fc2064920022c5d3c7
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-17 15:08:36 +0500

    done

--- main.cpp
+++ main.cpp
@@ -1,50 +1,22 @@
+#include <cstdint>
 #include <iostream>
 
-bool isAvailable(char a, char b)  {
-    return a >= 'a' && a <= 'h' && b >= '1' && b <= '8';
-}
-
 int main(int, char**) {
-    std::size_t querryCount;
-    std::cin >> querryCount;
-    std::cin.ignore(1);
+    int a, b, c;
 
-    std::string line;
-    while (querryCount) {
-        std::size_t availableSteps = 0;
+    std::cin >> a >> b >> c;
 
-        std::getline(std::cin, line);
+//    if (b != 0) {
+//        if (c == 1) {
+//            std::cout << a - b - c << std::endl;
+//        } else {
+//            std::cout << a - b * c << std::endl;
+//        }
+//    } else {
+//        std::cout << -c << std::endl;
+//    }
 
-        if (isAvailable(line[0] + 1, line[1] + 2)) {
-            ++availableSteps;
-        }
-        if (isAvailable(line[0] + 1, line[1] - 2)) {
-            ++availableSteps;
-        }
-        if (isAvailable(line[0] - 1, line[1] + 2)) {
-            ++availableSteps;
-        }
-        if (isAvailable(line[0] - 1, line[1] - 2)) {
-            ++availableSteps;
-        }
-        if (isAvailable(line[0] + 2, line[1] - 1)) {
-            ++availableSteps;
-        }
-        if (isAvailable(line[0] + 2, line[1] + 1)) {
-            ++availableSteps;
-        }
-        if (isAvailable(line[0] - 2, line[1] + 1)) {
-            ++availableSteps;
-        }
-        if (isAvailable(line[0] - 2, line[1] - 1)) {
-            ++availableSteps;
-        }
-
-        std::cout << availableSteps << '\n';
-
-        --querryCount;
-    }
-    std::cout << std::flush;
+    std::cout << std::min(a - b * c, a - b - c) << std::endl;
 
     return 0;
 }
