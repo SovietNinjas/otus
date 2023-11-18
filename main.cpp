ff7ea9c8ab8cd0baebeab3bf2397ae83d088052b
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2023-11-18 11:46:46 +0500

    done

--- main.cpp
+++ main.cpp
@@ -2,15 +2,13 @@
 #include <array>
 
 int main(int, char**) {
-    std::array<std::pair<int, int>, 3> buff;
-    std::cin >> buff[0].first >> buff[0].second >> buff[1].first >> buff[1].second >> buff[2].first >> buff[2].second;
 
-    auto strawberriesHeight = buff[1].first - buff[2].first;
 
-    auto second = strawberriesHeight - (buff[0].first - buff[2].first);
-    auto first = strawberriesHeight - second;
+    int n, m;
 
-    std::cout << first << ' ' << second << std::endl;
+    std::cin >> n >> m;
+
+    std::cout << n * (m + 1) << std::endl;
 
     return 0;
 }
