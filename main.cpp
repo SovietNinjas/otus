c66fa842968a725f5f4d59431f93f818a061648f
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2024-04-17 13:32:11 +0500

    1ая итерация

--- main.cpp
+++ main.cpp
@@ -1,90 +1,9 @@
-#include <chrono>
-#include <fstream>
 #include <iostream>
-#include <sstream>
 
-class FileLogger {
-   public:
-    explicit FileLogger(const std::string& name) : ofs_(name + getCurrentTime_()) {}
+#include "cmd_mgr.h"
 
-    FileLogger& operator<<(std::string_view sv) {
-        if (ofs_.is_open()) {
-            ofs_ << sv;
-        }
-        return *this;
-    }
-
-    inline bool empty() const { return ofs_.rdbuf()->in_avail() == 0; }
-
-    inline void print() const { std::cout << ofs_.rdbuf(); }
-
-    void close() { ofs_.close(); }
-
-   private:
-    std::string getCurrentTime_() const {
-        using namespace std::chrono;
-        auto time = system_clock::to_time_t(system_clock::now());
-        return std::to_string(time);
-    }
-
-    std::ofstream ofs_{};
-};
-
-class CommandMgr {
-   public:
-    explicit CommandMgr(std::size_t maxSize) : maxSize_(maxSize) {}
-    ~CommandMgr() {
-        if (nestLevel_ == 0) endBlockBehavior_();
-    }
-
-    void pushCmd(std::string_view cmd) {
-        if (cmd == "{") {
-            if (++nestLevel_ == 1) {
-                endBlockBehavior_();
-            }
-        } else if (cmd == "}") {
-            if (--nestLevel_ == 0) {
-                endBlockBehavior_();
-            }
-        } else {
-            saveCmd_(cmd);
-
-            if (nestLevel_ == 0 && cmdCount_ == maxSize_) {
-                endBlockBehavior_();
-            }
-        }
-    }
-
-   private:
-    inline void endBlockBehavior_() {
-        if (!logger_.empty()) {
-            logger_ << "\n";
-            logger_.print();
-            logger_.close();
-            cmdCount_ = 0;
-        }
-    }
-
-    inline void saveCmd_(std::string_view cmd) {
-        if (logger_.empty()) {
-            logger_ << "bulk: " << cmd;
-            createNewFile();
-        } else {
-            logger_ << ", " << cmd;
-        }
-        ++cmdCount_;
-    }
-
-    inline void createNewFile() { logger_ = FileLogger("bulk"); }
-
-    std::size_t nestLevel_ = 0, cmdCount_ = 0, maxSize_ = 0;
-    FileLogger logger_{""};
-};
-
-int main(int, char**) {
-    std::size_t querryCount = 0;
-
-    std::cin >> querryCount;
+int main(int, char* argv[]) {
+    std::size_t querryCount = std::stoi(argv[1]);
 
     CommandMgr cmdMgr(querryCount);
 
