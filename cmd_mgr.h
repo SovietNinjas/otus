cf7f19192b31538d153ba4e537bd884ecda0ff33
Айрат Лутфуллин <LutfullinAF@sinara-group.com>
2024-04-29 18:29:22 +0500

    WIP

---
+++ cmd_mgr.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <sstream>
+#include <chrono>
+#include <fstream>
+
+class FileLogger {
+   public:
+    FileLogger& operator<<(std::string_view sv) {
+        ss_ << sv;
+        return *this;
+    }
+
+    inline void print() const {
+        std::cout << ss_.str();
+        saveToFile(ss_.str());
+    }
+
+    inline void createNew(const std::string& fileName) {
+        ss_.str("");
+        fileName_ = fileName + getCurrentTime_();
+    }
+
+   private:
+    std::string getCurrentTime_() const {
+        using namespace std::chrono;
+        auto time = system_clock::to_time_t(system_clock::now());
+        return std::to_string(time);
+    }
+
+    inline void saveToFile(std::string_view sv) const {
+        std::ofstream ofs(fileName_);
+        if (ofs.is_open()) {
+            ofs << sv;
+            ofs.close();
+        }
+    }
+
+    std::string fileName_;
+    std::stringstream ss_;
+};
+
+class CommandMgr {
+   public:
+    explicit CommandMgr(std::size_t maxSize) : maxSize_(maxSize) {}
+    ~CommandMgr() {
+        if (nestLevel_ == 0) endBlockBehavior_();
+    }
+
+    void pushCmd(std::string_view cmd) {
+        if (cmd == "{") {
+            if (++nestLevel_ == 1) {
+                endBlockBehavior_();
+            }
+        } else if (cmd == "}") {
+            if (--nestLevel_ == 0) {
+                endBlockBehavior_();
+            }
+        } else {
+            saveCmd_(cmd);
+
+            if (nestLevel_ == 0 && cmdCounter_ == maxSize_) {
+                endBlockBehavior_();
+            }
+        }
+    }
+
+   private:
+    inline void endBlockBehavior_() {
+        if (cmdCounter_ != 0) {
+            cmdCounter_ = 0;
+            logger_ << "\n";
+            logger_.print();
+        }
+    }
+
+    inline void saveCmd_(std::string_view cmd) {
+        if (cmdCounter_ == 0) {
+            resetLogger_();
+            logger_ << "bulk: " << cmd;
+        } else {
+            logger_ << ", " << cmd;
+        }
+        ++cmdCounter_;
+    }
+
+    inline void resetLogger_() { logger_.createNew("bulk"); }
+
+    std::size_t nestLevel_ = 0, maxSize_ = 0, cmdCounter_ = 0;
+    FileLogger logger_;
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
+#include "cmd.h"
+#include "writer_thread.h"
+#include "async.h"
+//-----------------------------------------------------------------------------
+int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv)
+{
+    unsigned int bulk = 5;
 
-int main(int, char **) {
+    async::handle_t h1 = async::connect(bulk);
+    async::handle_t h2 = async::connect(bulk);
+
+    async::receive(h1, "1", 1);
+    async::receive(h2, "1\n", 2);
+    async::receive(h1, "\n2\n3\n4\n5\n6\n{\na\n", 15);
+    async::receive(h1, "b\nc\nd\n}\n89\n", 11);
+
+    async::disconnect(h1);
+    async::disconnect(h2);
+
     return 0;
 }
+//-----------------------------------------------------------------------------
